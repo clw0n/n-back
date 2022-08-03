384eed75a0755c2562fda9efdaa76c37395e0632
b00k00doy_ <102551574+clw0n@users.noreply.github.com>
2022-08-03 16:48:36 +0200

    Update n-back.py
    Fixed a few bugs (game not finishing, looping to the infinite)
    Bugs remaining to fix : accuracy calculates up to y=1, but we need to ignore the first y value equal to n --as a result accuracy doesn't work
    implementing non visual timer soon

--- n-back.py
+++ n-back.py
@@ -1,4 +1,4 @@
-import os, sys, random
+import os, sys, random, sqlite3
 from time import sleep
 
 #make user select game mode
@@ -26,73 +26,86 @@ def select():
 
 #single-n-back
 def start_single():
+    #declaring the variables first
+    multiple = 2
     n = 1
-    y = 0
-    fails = []
-    compare_list = []
+    i = 0
     stats = []
-    #clear the screen first
-    os.system("clear")
-    while not y == 20 + n:
+    compare_list = []
+    while i <= 20 + n:
         grid = [[0 for i in range(3)] for i in range(3)]
         clear_screen()
-        rand_num = random.randint(0,2)
-        rand_num2 = random.randint(0,2)
-        grid[rand_num][rand_num2] = "X"
-        compare_list.append(rand_num)
-        compare_list.append(rand_num2)
+        randint_1 = random.randint(0,2)
+        randint_2 = random.randint(0,2)
+        grid[randint_1][randint_2] = "x"
+        compare_list.append(randint_1)
+        compare_list.append(randint_2)
+        #printing the grid
         for elem in grid:
             print(elem)
-        answer = input("> ")
-        if answer == "A" or "a":
-            try:
-                if not compare_list == []:
-                    if compare_list[-1] and compare_list[-2] == compare_list[-1 - n] and compare_list[-2 - n]:
-                        stats.append(1)
-                        start_single()
-                        y += 1
-                    else:
-                        stats.append(0)
-                        y += 1
-            except IndexError:
-                start_single()
-                y += 1
-
+        if len(compare_list) == multiple:
+            clear_screen()
+        else:
+            answer = int(input("> "))
+            if answer == 1:
+                if compare_list[-1] and compare_list[-2] == compare_list[-2 - n] and compare_list[-3 - n]:
+                    stats.append(1)
+                else:
+                    stats.append(0)
+            elif answer == 0:
+                if compare_list[-1] and compare_list[-2] != compare_list[-2 - n] and compare_list[-3 - n]:
+                    stats.append(1)
+                else:
+                    stats.append(0)
+        i += 1
+    print(compare_list, stats)
     #returning statistics
     posi = []
+    fails = []
     for elem in stats:
         if elem == 1:
             posi.append(elem)
     accuracy = len(posi) / len(stats) * 100
-    print("Your score : " + accuracy)
+    print("Game stats :\n")
+    print(f"Accuracy : {accuracy} %") 
     if 50.0 <= accuracy < 80.0:
         print("N-Back level is maintained. You need + 80% to increase your score.")
     elif accuracy >= 80.0:
         print("N-Back level increased ! Good job !")
         n += 1
+        multiple *= 2
     else:
         if len(fails) <3:
             fails.append(0)
-            print("N-Back level below 50% ! Only " + 3 - len(list) + " tries and N-Back level will be decreased !")
+            print(f"N-Back level below 50% ! Only {3 - len(fails)} tries left and N-Back level will be decreased !")
         else:
             if not n == 1:
                 n -= 1
+                multiple /= 2
                 print("N-Back level has been decreased.")
             else:
                 print("N-Back level is staying at 1.")
 
-
-def clear_screen():
-    os.system("clear")
-
-def log():
-    #note time at which user launched n-back
-    #gather all data from current activity
-    #put everything in a log file
-    #use data to create graph statistics .py script
+def interface():
+    #create well looking interfaces for the n-back and store the different variables inside a dictionnary
     pass
 
-#create class / main.py
+def log_data():
+    #if the player doesn't play, no log will be created
+    #note time at which user launched n-back
+    date = os.popen("date") # + end game date (convert time = time activity)
+
+    #gather all data from current activity 
+    #put everything in a log file after session is interrupted
+    #transfer data into a local database
+    pass
+
+def clear_screen():
+    os.system("clear -x")
+
+def settings():
+    pass
+    #change parameters : key bindings, erase all data, create a new user, change the interface
 
 if __name__ == "__main__":
     select()
