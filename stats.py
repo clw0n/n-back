62c2c14e784c191405919546adc937e580b373e0
b00k00doy_ <102551574+clw0n@users.noreply.github.com>
2022-08-03 14:03:34 +0200

    Update stats.py
    fixed log mistake

--- stats.py
+++ stats.py
@@ -1,4 +1,28 @@
+import sys, os
+
+#check user privileges
+if not os.geteuid() == 0:
+    sys.exit("You need to be root in order to save your progress !")
+
 #read the log data
-#separate, date, time spent, levels increased
-#put data into a graph, with a graphical user interface, with a curve for each game, the x axis as the time and the y for the N,
-#all on a graphical user interface
+def check_directory():
+    pwd = os.getcwd()
+    print("Checking log files...")
+    try:
+        if "n-back" in pwd:
+            os.mkdir("__log__")
+            print("Wrote log folder in current working directory. Do not remove the log files in the folder, for which they will be used to make a graph out of your n-back performances.")
+        else:
+            sys.exit("Please cd in the cloned github repository.")
+    except FileExistsError:
+        print("__log__ folder already exists.")
+
+        
+#connect to the database to retrieve data (date, levels increased, average games per day and game type)
+
+
+#use retrieved data to make a graph on tkinter with matplotlib for the graph
+
+
+if __name__ == "__main__":
+  check_directory()
